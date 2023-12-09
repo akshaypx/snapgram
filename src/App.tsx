33135733db1eeb0f0fccb9fae7904837c11cc614
NAGARRO\akshaysrivastava <akshay.srivastava@nagarro.com>
2023-12-09 18:45:18 +0530

    routing done

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,19 @@ import SigninForm from "./_auth/forms/SigninForm";
 import SignupForm from "./_auth/forms/SignupForm";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
-import { Home } from "./_root/pages";
+import {
+  AllUsers,
+  CreatePost,
+  EditPost,
+  Explore,
+  Home,
+  Profile,
+  Saved,
+  UpdateProfile,
+} from "./_root/pages";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import PostDetails from "./_root/pages/PostDetails";
 
 const App = () => {
   return (
@@ -19,6 +29,14 @@ const App = () => {
         {/* private routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
       <Toaster />
